dea6d62d36d887ec78758c85df76b74f7af868f6
Modern-coding <coolman962@yahoo.com>
2025-02-06 12:19:12 +0500

    all funcitonalities are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,65 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [principale, setPrincipale] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [years, setYears] = useState(0);
+  const [emi, setEMI] = useState(0);
+  function handleChange(e) {
+    let id = e.target.id;
+    let value = e.target.value;
+
+    if (id === "principale") setPrincipale(value);
+    if (id === "interest") setInterest(value);
+    if (id === "years") setYears(value);
+    console.log(principale, interest, years);
+    if (principale && interest && years) {
+      let r = interest;
+      r = r / 12 / 100; // per month
+      console.log(r);
+      const calcPow = Math.pow(1 + r, years * 12);
+      const amount = principale * ((r * calcPow) / (calcPow - 1));
+      setEMI(Math.round(amount));
+    }
+  }
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="loan-calc">
+        <h1>Mortgage Caclulator</h1>
+        <div className="inputs">
+          <label htmlFor="principale">Principale:</label>
+          <br />
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <input onChange={handleChange} type="number" id="principale" />
+          <br />
+
+          <label htmlFor="interest">Interest:</label>
+          <br />
+
+          <input onChange={handleChange} type="number" id="interest" />
+
+          <br />
+          <label htmlFor="years">Years:</label>
+          <br />
+
+          <input onChange={handleChange} type="number" id="years" />
+
+          <br />
+        </div>
+        <div className="output">
+          Your EMI is <b>${emi}</b>
+          <div>
+            Your principale amout is: <b>{principale}</b>
+          </div>
+          <div>
+            Your interest rate is: <b>{interest}</b>
+          </div>
+          <div>
+            your selectd years: <b>{years}</b>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
